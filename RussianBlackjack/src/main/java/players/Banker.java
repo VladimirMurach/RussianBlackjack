e2d689d259d1b163cea9019f848c93897deba5ba
VladimirMurach <vladimirmurach@gmail.com>
2024-06-15 13:03:12 +0300

    Расширены классы пользователь и банкир

--- RussianBlackjack/src/main/java/players/Banker.java
+++ RussianBlackjack/src/main/java/players/Banker.java
@@ -1,5 +1,23 @@
 package players;
 
+import cards.Card;
+import java.util.ArrayList;
+
 public class Banker extends Player {
+    
+    private ArrayList<Card> deck;
+
+    public Banker(ArrayList<Card> deck) {
+        this.deck = deck;
+    }
+
+    public ArrayList<Card> getDeck() {
+        return deck;
+    }
+
+    public void setDeck(ArrayList<Card> deck) {
+        this.deck = deck;
+    }
+    
     
 }
--- RussianBlackjack/src/main/java/players/User.java
+++ RussianBlackjack/src/main/java/players/User.java
@@ -1,5 +1,40 @@
 package players;
 
 public class User extends Player {
-    
+
+    private int victories;
+    private int defeats;
+    private int money;
+
+    public User(int victories, int defeats, int money) {
+        super();
+        this.victories = victories;
+        this.defeats = defeats;
+        this.money = money;
+    }
+
+    public int getVictories() {
+        return victories;
+    }
+
+    public void setVictories(int victories) {
+        this.victories = victories;
+    }
+
+    public int getDefeats() {
+        return defeats;
+    }
+
+    public void setDefeats(int defeats) {
+        this.defeats = defeats;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
+    }
+
 }
