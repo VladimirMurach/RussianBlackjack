72c56bcae05a25ce9c8a65440d7b51486bf3bc62
VladimirMurach <vladimirmurach@gmail.com>
2024-06-15 17:44:23 +0300

    Добавлены классы Game и XlsxReader

---
+++ RussianBlackjack/src/main/java/main/Game.java
@@ -0,0 +1,38 @@
+package main;
+
+import cards.Card;
+import cards.Isotope;
+import java.util.ArrayList;
+import players.Banker;
+import players.Opponent;
+import players.Player;
+import players.User;
+
+public class Game {
+    
+    private User user;
+    private Banker banker;
+    private ArrayList<Opponent> opponents;
+    private ArrayList<Player> players;
+    private XlsxReader reader;
+    private ArrayList<Isotope> isotopes;
+    private ArrayList<Card> deck;
+    
+    public void start() {
+        
+    }
+    
+    public void setDeck() {
+        
+    }
+    
+    public void setOpponents() {
+        
+    }
+
+    public User getUser() {
+        return user;
+    }
+    
+    
+}
