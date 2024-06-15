c5f45911d57eb56467c6ca9c2a4b64fb8c0d4303
VladimirMurach <vladimirmurach@gmail.com>
2024-06-15 16:22:29 +0300

    Банкир тасует колоду и раздает карты

--- RussianBlackjack/src/main/java/players/Banker.java
+++ RussianBlackjack/src/main/java/players/Banker.java
@@ -2,9 +2,10 @@ package players;
 
 import cards.Card;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Banker extends Player {
-    
+
     private ArrayList<Card> deck;
 
     public Banker(ArrayList<Card> deck) {
@@ -18,6 +19,13 @@ public class Banker extends Player {
     public void setDeck(ArrayList<Card> deck) {
         this.deck = deck;
     }
-    
-    
+
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
+    }
+
+    public Card dealCard() {
+        return deck.removeFirst();
+    }
+
 }
