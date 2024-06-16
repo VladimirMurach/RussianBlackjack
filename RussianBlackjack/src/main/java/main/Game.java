de3b716ff14d132b83bf996b492ad8719133cfd0
VladimirMurach <vladimirmurach@gmail.com>
2024-06-16 11:18:01 +0300

    Добавлено чтение колоды

--- RussianBlackjack/src/main/java/main/Game.java
+++ RussianBlackjack/src/main/java/main/Game.java
@@ -9,7 +9,7 @@ import players.Player;
 import players.User;
 
 public class Game {
-    
+
     private User user;
     private Banker banker;
     private ArrayList<Opponent> opponents;
@@ -17,22 +17,55 @@ public class Game {
     private XlsxReader reader;
     private ArrayList<Isotope> isotopes;
     private ArrayList<Card> deck;
-    
+
+    public Game() {
+        reader = new XlsxReader();
+        deck = new ArrayList<>();
+        readIsotopes();
+        setDeck();
+        readDeck();
+    }
+
+    public void readIsotopes() {
+        isotopes = reader.readIsotopes();
+    }
+
+    public void setDeck() {
+        ArrayList<Card> cards = new ArrayList<>();
+        cards.add(new Card("Шесть", 6, null));
+        cards.add(new Card("Семь", 7, null));
+        cards.add(new Card("Восемь", 8, null));
+        cards.add(new Card("Девять", 9, null));
+        cards.add(new Card("Десять", 10, null));
+        cards.add(new Card("Валет", 2, null));
+        cards.add(new Card("Дама", 3, null));
+        cards.add(new Card("Король", 4, null));
+        cards.add(new Card("Туз", 11, null));
+        for (Card card : cards) {
+            for (int i = 0; i < 4; i++) {
+                deck.add(card);
+            }
+        }
+    }
+
+    public void readDeck() {
+        deck = reader.readDeck(isotopes, deck);
+    }
+
     public void start() {
-        
+
     }
-    
+
     public void changeDeck() {
-        
+
     }
-    
+
     public void setOpponents() {
-        
+
     }
 
     public User getUser() {
         return user;
     }
-    
-    
+
 }
--- RussianBlackjack/src/main/java/main/XlsxReader.java
+++ RussianBlackjack/src/main/java/main/XlsxReader.java
@@ -1,5 +1,65 @@
 package main;
 
+import cards.Card;
+import cards.Isotope;
+import java.io.IOException;
+import java.util.ArrayList;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
 public class XlsxReader {
+
+    public ArrayList<Card> readDeck(ArrayList<Isotope> isotopes, ArrayList<Card> deck) {
+        Workbook workbook = null;
+        try {
+            workbook = new XSSFWorkbook("deck.xlsx");
+            Sheet sheet = workbook.getSheetAt(0);
+            int j = 0;
+            for (Row row : sheet) {
+                Isotope isotope = isotopes.get(Integer.parseInt(row.getCell(0).getStringCellValue()));
+                for (int i = j; i < 4 + j; i++) {
+                    deck.get(i).setIsotope(isotope);
+                    System.out.println(deck.get(i).getIsotope().getName());
+                }
+                j += 4;
+            }
+        } catch (IOException e) {
+
+        } finally {
+            try {
+                workbook.close();
+            } catch (IOException e) {
+
+            }
+        }
+        return deck;
+    }
     
+    public ArrayList<Isotope> readIsotopes() {
+        Workbook workbook = null;
+        ArrayList<Isotope> isotopes = new ArrayList<>();
+        try {
+            workbook = new XSSFWorkbook("isotopes.xlsx");
+            Sheet sheet = workbook.getSheetAt(0);
+            for (int i = 1; i < 21; i++) {
+                Row row = sheet.getRow(i);
+                String name = row.getCell(0).getStringCellValue();
+                double halfLife = row.getCell(1).getNumericCellValue();
+                double energy = row.getCell(2).getNumericCellValue();
+                isotopes.add(new Isotope(name, halfLife, energy));
+            }
+        } catch (IOException e) {
+
+        } finally {
+            try {
+                workbook.close();
+            } catch (IOException e) {
+
+            }
+        }
+        return isotopes;
+    }
 }
